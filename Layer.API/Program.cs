d9b60b39ac4527b3fa7671fef96369f10f0de063
KursatKaan <kursatkaanonaran@gmail.com>
2023-12-25 21:47:26 +0300

    Add Migrations

--- Layer.API/Program.cs
+++ Layer.API/Program.cs
@@ -1,3 +1,12 @@
+﻿using Layer.Core.Abstract.Repositories;
+using Layer.Core.Abstract.Services;
+using Layer.Core.Abstract.UnitOfWorks;
+using Layer.Repository;
+using Layer.Repository.Repositories;
+using Layer.Repository.UnitOfWorks;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +15,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+//builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
+    {
+        //Options.MigrationsAssembly("Layer.Repository"); Bu Type güvenli bir durum değil. Katmanın ismi değişirse sorun çıkarır.
+
+        option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name); //Bu sayede Type güvenli hale getirdik.
+    });
+});
+
+
 
 var app = builder.Build();
 
